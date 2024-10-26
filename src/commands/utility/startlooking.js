5cd74051ad08e6ef65c4af9b0e9118deccfb8150
WojMen <xwojmenx@gmail.com>
2024-10-26 22:07:59 +0200

    add: bot reads file from scrapper and sends new fortmatted posts to chat

--- src/commands/utility/startlooking.js
+++ src/commands/utility/startlooking.js
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from "discord.js";
 
-import { sleep } from "../../common/time";
+import * as fs from "node:fs";
+import { sleep } from "../../common/time.js";
 
 export default {
   data: new SlashCommandBuilder()
@@ -11,9 +12,21 @@ export default {
     await interaction.reply("Started looking! I will send messages every hour until you say `stop`.");
 
     try {
+      let counter = -1;
+      let lastMessageDateUTC = 1729970833;
+
       while (await stopLooking(interaction)) {
-        await interaction.channel.send("I'm still looking...");
-        await sleep(60000 * 60);
+        // await interaction.channel.send("I'm still looking...");
+
+        if (counter % 60 === 0 || counter === -1) {
+          console.log("Looking for new posts..." + new Date().toLocaleTimeString());
+          lastMessageDateUTC = await potentialPosts(lastMessageDateUTC, interaction);
+
+          counter = 0;
+        }
+
+        await sleep(1000);
+        counter++;
       }
     } catch (error) {
       console.error("Error in the looking loop:", error);
@@ -33,10 +46,57 @@ const stopLooking = async (interaction) => {
 
   const result = recentMessages.find((message) => {
     if (message.content.toLowerCase() === "stop" && !message.author.toString().includes("BeefWifHIV#")) {
-      console.log(false);
       return true;
     }
   });
-  console.log(result);
+
   return result ? false : true;
+};
+
+const potentialPosts = async (lastMessageDateUTC, interaction) => {
+  const outputFile = "src/data/biz.json";
+
+  const data = JSON.parse(fs.readFileSync(outputFile, "utf-8"));
+
+  const newPosts = data.posts.filter((post) => post.dateUTC > lastMessageDateUTC);
+
+  let messageContent = "";
+  const maxLength = 2000;
+
+  if (newPosts.length === 0) {
+    return lastMessageDateUTC;
+  }
+
+  await interaction.channel.send(`\n## --------------------------------------\n`);
+
+  for (const post of newPosts) {
+    lastMessageDateUTC = post.dateUTC > lastMessageDateUTC ? post.dateUTC : lastMessageDateUTC;
+
+    post.matchedKeywords.forEach((element) => {
+      post.postMessage = post.postMessage.toLowerCase().replace(element, `__**${element}**__`);
+    });
+
+    let messagePart = "";
+    messagePart += `### New post detected, keywords: __${post.matchedKeywords.join(", ")}__\n`;
+    messagePart += `--------------------------------------\n`;
+    messagePart += `**Link:** <${post.postLink}>\n`;
+    messagePart += `**Time:** ${post.dateTime}\n`;
+    messagePart += `**Message:** ${post.postMessage}\n`;
+
+    // Check if adding the new message part would exceed the limit
+    if (messageContent.length + messagePart.length > maxLength) {
+      // Send the current message content and reset
+      console.log("Sending message part:", messagePart);
+      await interaction.channel.send(messageContent);
+      messageContent = "";
+    } else {
+      messageContent += messagePart;
+    }
+  }
+
+  // Send the entire message at once
+  console.log("Sending message content:", messageContent);
+  if (messageContent.trim().length > 0) await interaction.channel.send(messageContent);
+
+  return lastMessageDateUTC;
 };
